2af4582d7efd3e703fa32fdf16e699515bee38c4
Gustavo Halperin <ideastouch@gmail.com>
2012-03-23 13:29:56 -0300

    Add PickerViewDataSurceBackground and UIView+utils to project and a new category NSData+utils.

---
+++ 3rdPart/Utils/Categories/Foundation/NSData+utils.h
@@ -0,0 +1,13 @@
+//
+//  NSData+utils.h
+//  iPhoneUtils
+//
+//  Created by gustavo halperin on 3/23/12.
+//  Copyright (c) 2012 Three Pillard Global. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (utils)
+- (NSString *)hexadecimalString;
+@end
---
+++ 3rdPart/Utils/Categories/Foundation/NSData+utils.m
@@ -0,0 +1,35 @@
+//
+//  NSData+utils.m
+//  iPhoneUtils
+//
+//  Created by gustavo halperin on 3/23/12.
+//  Copyright (c) 2012 Three Pillard Global. All rights reserved.
+//
+
+#import "NSData+utils.h"
+
+@implementation NSData (utils)
+#pragma mark - String Conversion
+- (NSString *)hexadecimalString {
+    /* Returns hexadecimal string of NSData. Empty string if data is empty.   */
+    
+    const unsigned char *dataBuffer = (const unsigned char *)[self bytes];
+    
+    if (!dataBuffer)
+        return [NSString string];
+    
+    NSUInteger          dataLength  = [self length];
+    NSMutableString     *hexString  = [NSMutableString stringWithCapacity:(dataLength * 2)];
+    
+    for (int i = 0; i < dataLength; ++i)
+        [hexString appendString:[NSString stringWithFormat:@"%02x", (unsigned long)dataBuffer[i]]];
+    // [hexString appendFormat:@"%02.2hhx", dataBuffer[i]];
+    
+    return [NSString stringWithString:hexString];
+    
+    
+    //NSString *result = [[dataBuffer description] stringByReplacingOccurrencesOfString:@" " withString:@""];
+    //result = [result substringWithRange:NSMakeRange(1, [result length] - 2)];
+    //return result;
+}
+@end
